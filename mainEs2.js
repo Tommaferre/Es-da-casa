e458c97ec04ac3113787a2efc42527e0eb3ba8d8
tommaferre <tommaso.ferrero@edu-its.it>
2025-04-25 18:54:25 +0200

    js

--- mainEs2.js
+++ mainEs2.js
@@ -8,23 +8,23 @@ function useful() {
   
   /**
    * Cancels a scheduled function
-   * @param {number} timeoutId - The ID of the timeout to cancel
+   * @param {number} timer - The ID of the timeout to cancel
    * @returns {void}
    */
-  function cancelFunction(timeoutId) {
-    clearTimeout(timeoutId);
+  function cancelFunction(timer) {
+    clearTimeout(timer);
     console.log("function cancelled");
   }
   
   // Schedule the useful function to run after 10 seconds. 
   // This doesn't stop the execution of the code, 
   // it starts a counter in the "background" and continue with the code.
-  // It return a numeric ID that we store in the const timeoutId for a later reuse.
-  const timeoutId = setTimeout(useful, 10000);
+  // It return a numeric ID that we store in the const timer for a later reuse.
+  const timer = setTimeout(useful, 10000);
   
   // Cancel the scheduled function after 5 seconds
   setTimeout(() => {
-    cancelFunction(timeoutId);
+    cancelFunction(timer);
   }, 5000);
 
   // The function useful() so desn't print nothing because it will be cancelled before the time
