a0946cc7c706689a4c38f21cc442fbbe333649d7
tommaferre <tommaso.ferrero@edu-its.it>
2025-07-03 09:53:54 +0200

    esercitazione php

---
+++ esercitazione1_Ferrero/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tommaso.ferrero/esercitazione1_ferrero',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tommaso.ferrero/esercitazione1_ferrero' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
